6d38344d16d87978e376d6ff15442daf37483511
Matt Wisner <matt@detroitweb.company>
2017-09-03 11:41:52 -0400

    Slight code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ember-videojs-dash
 [![Build Status](https://travis-ci.org/mwisner/ember-videojs-dash.svg?branch=master)](https://travis-ci.org/mwisner/ember-videojs-dash)
+[![Ember Observer Score](https://emberobserver.com/badges/ember-videojs-dash.svg)](https://emberobserver.com/addons/ember-videojs-dash)
 
 Ember + https://github.com/videojs/videojs-contrib-dash
 
--- tests/dummy/app/components/video-test.js
+++ tests/dummy/app/components/video-test.js
@@ -16,14 +16,6 @@ export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
     videojs(this.elementId);
-    /**
-    player.ready(function() {
-      player.src({
-        src: 'https://s3.amazonaws.com/_bc_dml/example-content/sintel_dash/sintel_vod.mpd',
-        type: 'application/dash+xml'
-      });
-    });
-    **/
   }
 });
 
